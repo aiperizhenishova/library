e5dd501da3f912fe3b8539f4a9922a8fd3ca8752
Aiperi <122115154+aiperizhenishova@users.noreply.github.com>
2024-09-20 15:10:11 +0600

    Delete library directory

--- library/src/main/java/kg/alatoo/library/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package kg.alatoo.library.repositories;
-
-import kg.alatoo.library.entities.BookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<BookEntity, Long> {
-}
